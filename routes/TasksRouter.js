47d91341c498b98109c03dd5a7199d86a1e1fb95
kotypopadayutvray <blikylia@gmail.com>
2018-07-03 19:21:51 +0300

    Fix Tasks router

--- routes/TasksRouter.js
+++ routes/TasksRouter.js
@@ -4,12 +4,12 @@ var router = express.Router();
 var tasksController = require('../application/controllers/UserController');
 
 router.get('/', tasksController.todolists);
+router.put('/task/:id', tasksController.updateTask);
 router.post('/task', tasksController.createTask);
-router.update('/task/:id', tasksController.updateTask);
 router.delete('/task/:id', tasksController.deleteTask);
 router.get('/todolist/:id', tasksController.todolist);
+router.put('/todolist/:id', tasksController.updateTodolist);
 router.post('/todolist/:id', tasksController.createTodolist);
-router.update('/todolist/:id', tasksController.updateTodolist);
 router.delete('/todolist/:id', tasksController.deleteTodolist);
 
 module.exports = router;
