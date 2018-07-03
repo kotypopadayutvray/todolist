54d5961dcc68b54ce813ace3ab9eac4b869d7a03
kotypopadayutvray <blikylia@gmail.com>
2018-07-03 19:01:21 +0300

    Write controller's actions

--- application/controllers/TasksController.js
+++ application/controllers/TasksController.js
@@ -51,8 +51,15 @@ exports.createTodolist = function(request, response) {
 };
 
 exports.updateTodolist = function(request, response) {
-  models.Todolist.create(todolistData).then(todolist => {
-    response.redirect('/tasks/todolist/' + todolist.id);
+  models.Todolist.findById(request.params.id).then(todolist => {
+    todolist.name = request.params.name;
+    todolist.end_time = request.params.name;
+    todolist.save().then(() => {
+      response.redirect('/tasks/todolist/' + todolist.id);
+    }).catch(errors => {
+      console.log('Save todolist error. Info: ', errors);
+      response.render(viewPath + '../error.ejs', { errors: errors });
+    });
   }).catch(errors => {
     console.log('Get todolist error. Info: ', errors);
     response.render(viewPath + '../error.ejs', { errors: errors });
@@ -60,5 +67,11 @@ exports.updateTodolist = function(request, response) {
 };
 
 exports.deleteTodolist = function(request, response) {
-
+  models.Todolist.findById(request.params.id).then(todolist => {
+    todolist.destroy();
+    response.redirect('/tasks/todolists/');
+  }).catch(errors => {
+    console.log('Get todolist error. Info: ', errors);
+    response.render(viewPath + '../error.ejs', { errors: errors });
+  });
 };
