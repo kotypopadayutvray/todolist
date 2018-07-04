6051a68fec64363d41907707cc47786df8634e46
kotypopadayutvray <blikylia@gmail.com>
2018-07-04 19:03:47 +0300

    Add views

--- routes/TasksRouter.js
+++ routes/TasksRouter.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 var tasksController = require('../application/controllers/TasksController');
 
-router.get('/', tasksController.todolists);
-router.put('/task/:id', tasksController.updateTask);
-router.post('/task', tasksController.createTask);
-router.delete('/task/:id', tasksController.deleteTask);
-router.get('/todolist/:id', tasksController.todolist);
-router.put('/todolist/:id', tasksController.updateTodolist);
-router.post('/todolist/:id', tasksController.createTodolist);
-router.delete('/todolist/:id', tasksController.deleteTodolist);
+router.get('/', isAuthenticated, tasksController.todolists);
+router.put('/task/:id', isAuthenticated, tasksController.updateTask);
+router.post('/task', isAuthenticated, tasksController.createTask);
+router.delete('/task/:id', isAuthenticated, tasksController.deleteTask);
+router.get('/todolist/:id', isAuthenticated, tasksController.todolist);
+router.put('/todolist/:id', isAuthenticated, tasksController.updateTodolist);
+router.post('/todolist', isAuthenticated, tasksController.createTodolist);
+router.delete('/todolist/:id', isAuthenticated, tasksController.deleteTodolist);
+
+function isAuthenticated(request, response, next) {
+  if (request.isAuthenticated()) {
+    return next();
+  }
+  response.redirect('/');
+}
 
 module.exports = router;
